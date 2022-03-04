6f8c964b24b449813f0229f692de562aabf37fbb
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2022-03-05 00:55:48 +0100

    Added fallback for caching

--- sw.js
+++ sw.js
@@ -4,12 +4,29 @@ const cacheName = 'news-v1';
 // SW should cache all our static assets
 
 // defined array of static assets
-const staticAssets = ['./','./styles.css','./app.js']
+const staticAssets = [
+    './',
+    './styles.css',
+    './app.js'
+];
 
-self.addEventListener('install', async function () {
+self.addEventListener('install', async e => {
+    console.log('install', e);
     const cache = await caches.open(cacheName);
     cache.addAll(staticAssets);
 });
 
-self.addEventListener('fetch', e => console.log('fetch', e));
+// Events sent from application to network
+// Intercepting fetch events
+self.addEventListener('fetch', e => {
+    console.log('fetch', e);
+    const req = e.request;
+    e.respondWith(cacheFirst(req));
+});
+
+async function cacheFirst(req) {
+    const cachedResponse = await caches.match(req);
+    // Fallback if not cached
+    return cachedResponse || fetch(req);
+}
 
